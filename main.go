15d31e5753b27e5008c08b4fcde6481a58de32ae
Ben Johnson <bjohnson@binarylogic.com>
2019-05-20 12:23:35 -0400

    Make address an argument

--- main.go
+++ main.go
@@ -9,41 +9,33 @@ import (
 
 func main() {
 	app := cli.NewApp()
-	app.Name = "tcp_terminator"
+	app.Name = "tcp_test_server"
 	app.Usage = "Simple TCP server that is useful for testing purposes."
-
-	app.Commands = []cli.Command{
-		{
-			Name:      "start",
-			Usage:     "Start the TCP server and listen for messages",
-			ArgsUsage: "[addr]",
-			Flags: []cli.Flag{
-				cli.StringFlag{
-					Name:  "file, f",
-					Usage: "A file to write messages to",
-				},
-			},
-			Action: func(ctx *cli.Context) error {
-				addr := ctx.Args().Get(0)
-
-				if addr == "" {
-					message := "The address argument is required: `tcp_terminator 0.0.0.0:9000`"
-					// Exit with 65, EX_DATAERR, to indicate input data was incorrect
-					return cli.NewExitError(message, 65)
-				}
-
-				file := ctx.String("file")
-				server := NewServer(addr, file)
-				server.Listen()
-
-				if server.File != nil {
-					log.Println("Closing file")
-					server.File.Close()
-				}
-
-				return nil
-			},
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:  "file, f",
+			Usage: "A file to write messages to",
 		},
+	}
+	app.Action = func(ctx *cli.Context) error {
+		addr := ctx.String("address")
+
+		if addr == "" {
+			message := "The address argument is required: `tcp_test_server -a 0.0.0.0:9000`"
+			// Exit with 65, EX_DATAERR, to indicate input data was incorrect
+			return cli.NewExitError(message, 65)
+		}
+
+		file := ctx.String("file")
+		server := NewServer(addr, file)
+		server.Listen()
+
+		if server.File != nil {
+			log.Println("Closing file")
+			server.File.Close()
+		}
+
+		return nil
 	}
 
 	err := app.Run(os.Args)
